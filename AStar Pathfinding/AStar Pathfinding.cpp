9bed256c1a4aecbc193f6339da3c6fb35cde8327
Gabriel CHIA <gabriel_chia@NECSAPSIN.COM>
2020-09-22 15:19:12 +0800

    Build H Cost

--- AStar Pathfinding/AStar Pathfinding.cpp
+++ AStar Pathfinding/AStar Pathfinding.cpp
@@ -12,6 +12,8 @@ using namespace std;
 
 const int x = 10;
 const int y = 10;
+int startNode, endNode = -1;
+
 
 class point {
 public:
@@ -19,12 +21,24 @@ public:
 	int downNeighbour;
 	int leftNeighbour;
 	int rightNeighbour;
-	float pointValue;
+
+	bool oList;
+	bool cList;
+	int hCost;
+	int gCost;
+	int fCost;
+
+	point() {
+		upNeighbour = -1;
+		downNeighbour = -1;
+		leftNeighbour = -1;
+		rightNeighbour = -1;
+	}
 };
 
 struct orderPointValue
 {
-	bool operator() (point const &a, point const &b) { return a.pointValue > b.pointValue; }
+	bool operator() (point const &a, point const &b) { return a.fCost > b.fCost; }
 };
 
 bool retCoords(int id, int* arr) {
@@ -36,13 +50,33 @@ bool retCoords(int id, int* arr) {
 	return true;
 }
 
+int calculateHCost(int id) {
+	if (id < 0 || id >= x*y)
+		return;
+
+	int coords0[2];
+	int coords1[2];
+
+	retCoords(id, coords0);
+	retCoords(endNode, coords1);
+
+	int xDiff, yDiff = 0;
+
+	xDiff = abs(coords1[1] - coords0[1]);
+	yDiff = abs(coords1[0] - coords0[0]);
+
+	int hCost = (xDiff * xDiff) + (yDiff * yDiff);
+
+	// returns F which is H + G
+	return hCost;
+}
 
 int main()
 {
 	priority_queue <point, vector<point>, orderPointValue> nextPoint;
 	unordered_map<int, point> map;
 
-	float pointData[y][x] = {
+	int pointData[y][x] = {
 		{ 0,0,0,0,0,0,0,0,0,2 },
 		{ 0,0,0,0,0,0,0,0,0,0 },
 		{ 0,0,0,0,0,0,0,0,0,0 },
@@ -51,9 +85,11 @@ int main()
 		{ 0,0,0,0,0,0,0,0,0,0 },
 		{ 0,0,0,0,0,0,0,0,0,0 },
 		{ 0,0,0,0,0,0,0,0,0,0 },
-		{ 0,0,0,0,0,0,0,0,0,0 },
-		{ 1,0,0,0,0,0,0,0,0,0 }
+		{ 0,1,0,0,0,0,0,0,0,0 },
+		{ 0,0,0,0,0,0,0,0,0,0 }
 	};
+
+
 
 	for (int i = 0; i < y; i++)
 		for (int j = 0; j < x; j++) {
@@ -67,23 +103,46 @@ int main()
 			if (leftCell > -1) {
 				map[leftCell].rightNeighbour = idVal;
 				p.leftNeighbour = leftCell;
-				cout << "Transaction horizontal with " << idVal << " and " << leftCell << endl;
+				//cout << "Transaction horizontal with " << idVal << " and " << leftCell << endl;
 			}
 
 			if (downCell > -1) {
 				map[downCell].upNeighbour = idVal;
 				p.downNeighbour = downCell;
-				cout << "Transaction vertical with " << idVal << " and " << downCell << endl;
+				//cout << "Transaction vertical with " << idVal << " and " << downCell << endl;
 			}
 
 			map.insert(pair<int, point>(idVal, p));
+
+			switch (pointData[i][j]) {
+			case 0:
+				startNode = idVal;
+				break;
+			case 1:
+				endNode = idVal;
+				break;
+			}
 		}
+
+	if (startNode == -1 || endNode == -1) {
+		cout << "sN or eN has not been set! Ending process...";
+		system("pause");
+		return 0;
+	}
+
+	map[startNode].fCost = 0;
+	nextPoint.push(map[startNode]);
+
+	while (!nextPoint.empty()) {
+		point pInst = nextPoint.top();
+		
+	}
 
 	//for (int i = 0; i < y; i++)
 		//for (int j = 0; j < x; j++)
 			//cout << "Test val: " << map[(i*x) + j].pointValue << endl;
 
-	point p1 = point();
+	/*point p1 = point();
 	p1.pointValue = 5;
 
 	point p2 = point();
@@ -94,13 +153,14 @@ int main()
 
 	nextPoint.push(p1);
 	nextPoint.push(p2);
-	nextPoint.push(p3);
+	nextPoint.push(p3);*/
 
-	while (!nextPoint.empty()) {
-		cout << "P value " << nextPoint.top().pointValue << endl;
-		nextPoint.pop();
-	}
+	//while (!nextPoint.empty()) {
+		//cout << "P value " << nextPoint.top().totalCost << endl;
+		//nextPoint.pop();
+	//}
 
+	cout << "Path has been found!";
 	system("pause");
 
 	return 0;
